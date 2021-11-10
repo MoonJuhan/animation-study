e7334cc4f1e0522e509b73a03ee4fc53e5a7846a
MoonJuhan <codemike@naver.com>
2021-11-10 16:53:17 +0900

    fix: vite에서 vue-cli로 빌드 도구 수정

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,18 @@ export default {
 </script>
 
 <style lang="scss">
+#app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
 body {
   margin: 0;
 }
@@ -18,11 +30,5 @@ ul {
   margin: 0;
   padding: 0;
   list-style: none;
-}
-
-#app {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
 }
 </style>
