d79c699cccfa4698414b4c62c6642842135dd7f8
MoonJuhan <codemike@naver.com>
2021-11-21 17:20:15 +0900

    feat: fontawesome 추가

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 import store from './store'
 import plugins from './plugins'
 
+import '@fortawesome/fontawesome-free/js/all.js'
+require('@/assets/styles/index.scss')
+
 const boot = async () => {
   const app = createApp(App)
   app.use(store).use(router)
@@ -17,4 +20,3 @@ const boot = async () => {
 }
 
 boot()
-
